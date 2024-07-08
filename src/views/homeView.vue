dda8287154610d68df2adfff0978e58305bad0df
linush <linus_hakansson@outlook.com>
2024-07-08 10:37:54 +0200

    fixed the getCardText method

--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -2,7 +2,7 @@
   <navigationBar class="navigation margin" />
 
   <div class="container margin">
-    <card :name="getCardText" />
+    <card :name="getCardText()" />
   </div>
 </template>
 
@@ -25,11 +25,12 @@ export default {
 
   methods: {
     getCardText() {
-      return `
-        Mina tidigare arbetserfarenheter samt livserfarenheter har lärt 
-        mig att vara en god tillgång för gruppens bästa och en person som höjer 
-        stämningen som gärna gotta ner sig i problem som jag får lösa. 
-        Jag strävar emot att ständigt utvecklas och bli en bättre version av mig själv varje dag!`
+      const text = `
+        Hej! Jag heter Linus Håkansson och jag är en passionerad webbutvecklare med en kärlek för att skapa och utforska. När jag inte arbetar med att bygga webbsidor, finner du mig ofta med en gitarr i handen eller i köket där jag experimenterar med nya recept.
+        Min resa inom webbutveckling började när jag var 15 år gammal och byggde min första hemsida. Sedan dess har jag varit fast och älskar att dyka ner i nya teknologier och lösa komplexa problem. Jag har arbetat med flera projekt som har hjälpt mig att växa både professionellt och personligt.
+        Utöver webbutveckling är jag också en stor naturälskare. Jag tycker om att vandra, fotografera och tillbringa tid utomhus. Dessa upplevelser hjälper mig att hålla mig inspirerad och kreativ.
+        Denna hemsida har jag skapat för att dela med mig av mina projekt, mina tankar och för att du ska få en bättre bild av vem jag är som person. Jag hoppas att du finner något intressant här och tveka inte att kontakta mig om du vill veta mer eller bara säga hej!`
+      return text 
     }
   }
 }
