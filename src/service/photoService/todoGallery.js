5451262326ae85743d0a1e568314819522f21dcd
linush <linus_hakansson@outlook.com>
2024-07-09 11:25:17 +0200

    added galery to its own component and made it general.

---
+++ src/service/photoService/todoGallery.js
@@ -0,0 +1,9 @@
+import picture from '@/assets/picture.jpg'
+import picture2 from '@/assets/selfie.jpg'
+
+export default [
+  {src: picture},
+  {src: picture2},
+  {src: picture2},
+  {src: picture},
+]
--- src/views/projectsView.vue
+++ src/views/projectsView.vue
@@ -1,100 +1,29 @@
 <template>
   <drawer />
 
-  <div class="image-container">
-    <img v-for="(picture, index) in pictures" :key="index" 
-      :src="picture" alt="Image" 
-      @click="showPreview(picture)">
-  </div>
-  
-  <div class="modal" v-if="previewImage">
-    <span class="close" @click="closePreview"> &times; </span>
-    <img :src="previewImage" alt="Preview">
-  </div>
+  <gallery :images="images" />
 </template>
 
 <script>
-import picture from '../assets/picture.jpg'
 import drawer from '../components/drawer.vue'
+import gallery from '../components/gallery.vue'
 
+import images from '@/service/photoService/todoGallery.js'
 
 export default {
+
   data() {
     return {
-      pictures: [picture, picture, picture, picture],
-      previewImage: ''
+      images
     }
   },
 
   components: {
-    drawer
-  },
-
-  methods: {
-    showPreview(picture) {
-      this.previewImage = picture
-    },
-    
-    closePreview() {
-      this.previewImage = ''
-    }
+    drawer,
+    gallery
   }
 }
 </script>
 
 <style>
-.image-container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-.image-container img {
-  width: 200px;
-  height: 200px;
-  margin: 10px;
-  cursor: pointer;
-  border-radius: 5px;
-}
-
-.image-container img:hover {
-  border: 3px solid #8ecccc;
-  -webkit-animation-timing-function: linear;
-  -webkit-transition: ease-in-out 0.1s;
-}
-
-.modal {
-  display: block;
-  position: fixed;
-  z-index: 1000;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
-  background-color: rgba(0, 0, 0, 0.8);
-}
-
-.modal img {
-  margin: auto;
-  display: block;
-  max-width: 80%;
-  max-height: 80%;
-  border-radius: 5px;
-}
-
-.close {
-  color: #fff;
-  position: absolute;
-  top: 15px;
-  right: 35px;
-  font-size: 30px;
-  cursor: pointer;
-}
-
-.close:hover, .close:focus {
-  color: #ccc;
-  text-decoration: none;
-  cursor: pointer;
-}
 </style>
