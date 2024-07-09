0092ed220b09f085f39082a358dd132b3a55958d
linush <linus_hakansson@outlook.com>
2024-07-09 16:17:24 +0200

    adding dist sutree

---
+++ assets/homeView-wxe_3xFD.js
@@ -0,0 +1,12 @@
+import{n as s,a as m,c as i}from"./aboutMe-CvMdKGTQ.js";import{d}from"./selfie-CsXYgRee.js";import{_ as l,c,a,b as g,F as p,r as e,o as h}from"./index-iH-qDQyE.js";const k={data(){return{}},components:{navigationBar:s,aboutMe:m,card:i,Drawer:d},methods:{getCardText(){return`Hej!
+Jag heter Linus Håkansson och jag är en passionerad webbutvecklare med en kärlek för att skapa och utforska.
+När jag inte arbetar med att bygga webbsidor, finner du mig ofta på gymmet eller i någon idrottslokal.
+
+Min resa inom IT började när jag var 14-15 år gammal och byggde min första dator hemma tillsammans med min bror. Sedan dess har jag varit fast och älskar att dyka ner i nya teknologier och lösa problem.
+
+Jag har arbetat med flera projekt som har hjälpt mig att växa både professionellt och personligt.
+
+Utöver datorer, teknik och idrott är jag en också en stor naturälskare. Jag tycker om att tillbringa tid utomhus. Dessa upplevelser hjälper mig att hålla mig inspirerad och kreativ.
+
+Denna hemsida har jag skapat för att dela med mig av mina projekt, mina tankar och för att du ska få en bättre bild av vem jag är som person.
+Jag hoppas att du finner något intressant här och tveka inte att kontakta mig om du vill veta mer eller bara säga hej!`}}},f={class:"container margin"};function u(t,b,v,_,j,r){const o=e("Drawer"),n=e("card");return h(),c(p,null,[a(o),g("div",f,[a(n,{class:"pre-formatted",name:r.getCardText()},null,8,["name"])])],64)}const D=l(k,[["render",u]]);export{D as default};
