deab07c18a7a8c6b71c987877d1ab74204e2d8d7
linush <linus_hakansson@outlook.com>
2024-07-10 13:57:45 +0200

    adding dist

--- dist/assets/homeView-1k48ypdl.js
+++ dist/assets/homeView-1k48ypdl.js
@@ -1,4 +1,4 @@
-import{n as s,a as i,c as m}from"./contactView-BsJfkmWR.js";import{d}from"./progress-Ccm5tJCN.js";import{_ as l,c,a,b as g,F as p,r as e,o as h}from"./index-CEmemUQn.js";const k={data(){return{}},components:{navigationBar:s,aboutMe:i,card:m,Drawer:d},methods:{getCardText(){return`Hej!
+import{n as s,a as i,c as m}from"./contactView-CTsHAooV.js";import{d}from"./progress-DPjAyvBE.js";import{_ as c,c as l,a,b as g,r as e,o as p}from"./index-DbMJArBL.js";const h={data(){return{}},components:{navigationBar:s,aboutMe:i,card:m,Drawer:d},methods:{getCardText(){return`Hej!
 Jag heter Linus Håkansson och jag är en passionerad webbutvecklare med en kärlek för att skapa och utforska.
 När jag inte arbetar med att bygga webbsidor, finner du mig ofta på gymmet eller i någon idrottslokal.
 
@@ -9,4 +9,4 @@ Jag har arbetat med flera projekt som har hjälpt mig att växa både profession
 Utöver datorer, teknik och idrott är jag en också en stor naturälskare. Jag tycker om att tillbringa tid utomhus. Dessa upplevelser hjälper mig att hålla mig inspirerad och kreativ.
 
 Denna hemsida har jag skapat för att dela med mig av mina projekt, mina tankar och för att du ska få en bättre bild av vem jag är som person.
-Jag hoppas att du finner något intressant här och tveka inte att kontakta mig om du vill veta mer eller bara säga hej!`}}},f={class:"container margin"};function u(t,b,v,_,j,r){const n=e("Drawer"),o=e("card");return h(),c(p,null,[a(n),g("div",f,[a(o,{class:"pre-formatted",name:r.getCardText(),title:"Linus Håkansson"},null,8,["name"])])],64)}const D=l(k,[["render",u]]);export{D as default};
+Jag hoppas att du finner något intressant här och tveka inte att kontakta mig om du vill veta mer eller bara säga hej!`}}},k={class:"background-color"},f={class:"container margin"};function u(t,b,v,_,j,r){const o=e("Drawer"),n=e("card");return p(),l("div",k,[a(o),g("div",f,[a(n,{class:"pre-formatted",name:r.getCardText(),title:"Linus Håkansson"},null,8,["name"])])])}const D=c(h,[["render",u]]);export{D as default};
