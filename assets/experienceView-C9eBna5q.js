fdf63b0f938d7a3c45cb0651e68745863e4720f5
linush <linus_hakansson@outlook.com>
2024-07-15 12:08:07 +0200

    adding dist subtree

---
+++ assets/experienceView-C9eBna5q.js
@@ -0,0 +1,23 @@
+import{D as i}from"./progress-C44N0zir.js";import{c}from"./card-D9yKbrUo.js";import{_ as k,c as l,a as e,b as n,F as m,r as s,o as d}from"./index-IkDxNFUH.js";const g="/cv/assets/skolon-Beqc4s9S.jpg",p="/cv/assets/saabVisbyCorvett-CNFAdMar.jpg",u="/cv/assets/bredakraHalloween-B2gHLRzi.jpg",v={components:{card:c,Drawer:i},data(){return{imgSkolon:g,imgBredakra:u,imgSaab:p}},methods:{skolonText(){return`Under en av mina praktikperioder arbetade jag som fullstack webbutvecklare hos Skolon AB.
+      
+Tekinker som jag utvecklade lite mer under min tid hos Skolon var:
+    *   Java 17 och Spring Boot
+    *   JavaScript och Vue.js (Options API)
+    *   HTML och CSS
+    *   MySql
+    *   Postman
+    *   Table Plus
+    *   Git
+    *   Bitbucket
+    *   Wrike
+      
+En vanlig dag kunde vara standup möte, gå igenom vad som behövdes göras för dagen för varje specifik person. Ta emot tickets via Wrike och sedan utföra sina arbetsuppgifter inför dagen eller dagarna. Det kunde vara allt från att leta buggar till att implementera helt nya funktioner.
+
+När uppgiftens var klar och hade skickat in sina commits till Bitbucket så blev det alltid en code review där jag fick lära mig mycket genom konstruktiv kritik.`},saabText(){return`Under min utbildning som Javautvecklare lyckades jag få in en fot på Saab Kockums i Karlskrona. Där arbetade jag med IT-Säkerhet under en IT-Säkerhetsarkitekt.
+      
+Jag arbetade med verktyg som Jira och andra interna verktyg som Saab AB kunde erbjuda.
+
+Vi hade dagliga standups och fick jag fick lära mig om diverse olika informationssäkerhets principer och taktiker.
+
+På grund av säkerhetsklassningen kan jag inte gå in på så mycket detaljer.`},bredakraText(){return`Jag arbetade inom parkförvaltningen och tog hand om både parkarbete (grönytor) och begravningsverksamhet som t.ex. att hölja både jordgravar med kistor och urnsättningar. 
+Användning av olika maskiner förekom ofta som t.ex. Walker gräsklippar och Avant.`}}},b={class:"container margin-top"},h={class:"container spacing-around"},f={class:"container spacing-around"};function S(_,j,B,T,a,r){const o=s("Drawer"),t=s("card");return d(),l(m,null,[e(o),n("div",b,[e(t,{image:a.imgSkolon,title:"Skolon AB - Fullstack utvecklare (praktik)",name:r.skolonText(),class:"pre-formatted"},null,8,["image","name"])]),n("div",h,[e(t,{image:a.imgSaab,title:"Saab AB - IT säkerhetsspecialist (praktik)",name:r.saabText(),class:"pre-formatted"},null,8,["image","name"])]),n("div",f,[e(t,{image:a.imgBredakra,title:"Bredåkra - Säsongsanställning, grönytor",name:r.bredakraText(),class:"pre-formatted"},null,8,["image","name"])])],64)}const w=k(v,[["render",S]]);export{w as default};
