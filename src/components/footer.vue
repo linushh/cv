6e7e216a30113f3717b9aee085afef00246d23dd
linush <linus_hakansson@outlook.com>
2024-07-28 11:56:52 +0200

    fixed the spacing on the footer for the links

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,24 +1,23 @@
 <template>
 <footer>
   <div class="box">
+
     <div>
       <p class="title-size">Author</p>
       <p class="text-size">Linus Håkansson</p>
     </div>
     
-    <br>
-    
-    <div>
+    <div class="link-box">
       <p class="title-size">Links</p>
-      <p
-        class="text-size classname">
-        <a>Github</a>
-        <a>LinkedIn</a>
-        <a>Facebook</a>
-      </p>
-    </div>
 
-    <br>
+      <div class="links">
+        <a href="https://github.com/linushh"target="_blank" class="text-size">Github</a>
+
+        <a href="https://www.linkedin.com/in/linus-h%C3%A5kansson-4584ab251?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BGPilcz5JRvGutro67CmQ0A%3D%3D" target="_blank" class="text-size">LinkedIn</a>
+
+        <a href="https://www.facebook.com/linus.hakansson.98478" target="_blank" class="text-size">Facebook</a>
+      </div>
+    </div>
 
     <div>
       <p class="title-size">Made with</p>
@@ -47,8 +46,28 @@
   font-size: 1.2rem;
 }
 
-.classname a {
+.links a {
   background-color: #444;
   cursor: pointer;
+}
+
+.links a:hover {
+  text-decoration: underline;
+}
+
+.link-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.link-box p, .link-box a {
+  margin-left: 8px;
+  margin-right: 8px;
+  padding: 0;
+}
+
+.links {
+  display: flex;
 }
 </style>
