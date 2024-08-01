2325e18e36234d133695978534e1a0952b4131bc
linush <linus_hakansson@outlook.com>
2024-08-01 10:17:15 +0200

    added all the icons needed for my skills-view. Also added my-skills view in the contact view. Im about to rename contact view and make it my-skills instead

--- src/views/contactView.vue
+++ src/views/contactView.vue
@@ -1,20 +1,70 @@
 <template>
   <div class="background-color">
-
-    <div class="centering margin">
-    <card 
-      :image="image" 
-      class="preformatted" 
-      :name="getCardText()"
-      :title="'Contact information:'" >
-    </card>
+    <div class="centering">
+      <card
+        :image="java"
+        :title="'Java'"
+        :name="iconTexts('java')"
+        class="preformatted margin-between">
+      </card>
+      
+      <card
+        :image="springBoot"
+        :title="'Spring Boot'"
+        :name="iconTexts('springBoot')"
+        class="preformatted margin-between">
+      </card>
+    </div>
+      
+    <div class="centering">
+      <card 
+        :image="vue" 
+        :title="'Vue.js'"
+        :name="iconTexts('vue')"
+        class="preformatted margin-between">
+      </card>
+      
+      <card
+        :image="javascript"
+        :title="'JavaScript'"
+        :name="iconTexts('javascript')"    
+        class="preformatted margin-between">
+      </card>
+    </div>
+  
+    <div class="centering">
+      <card
+        :image="mysql"
+        :title="'MySQL'"
+        :name="iconTexts('mysql')"
+        class="preformatted margin-between">
+      </card>
+      
+      <card
+        :image="git"
+        :title="'Git'"
+        :name="iconTexts('git')"
+        class="preformatted margin-between">
+      </card>
   </div>
 </div>
 </template>
 
 <script>
+import springBoot from '@/assets/iconsMySkills/spring-boot.png'
+import java from '@/assets/iconsMySkills/java.png'
+
+import vueJs from '@/assets/iconsMySkills/vue-js.png'
+import javascript from '@/assets/iconsMySkills/javascript.png'
+import css from '@/assets/iconsMySkills/css.png'
+
+import mysql from '@/assets/iconsMySkills/mysql.png'
+
+import git from '@/assets/iconsMySkills/git.png'
+
 import selfie from '@/assets/selfie.jpg'
 import socials from '@/service/socials'
+
 
 import card from '../components/card.vue'
 
@@ -28,14 +78,49 @@ export default {
   data() {
     return {
       image: selfie,
-      socials: socialData
+      socials: socialData,
+
+      java: java,
+      springBoot: springBoot,
+      mysql: mysql,
+
+      javascript: javascript,
+      vue: vueJs,
+      css: css,
+
+      git: git,
     }
   },
 
   methods: {
-    getCardText() {
-      return `Email: ` + this.socials.email + ` 
-Phone: ` + this.socials.phoneNumber
+    iconTexts(name) {
+      const text = {
+        java: `Under min utbildning var vårt huvudspråk Java. Vi använde oss mesta dels utav nyare version som Java 17.
+På mitt tidigare arbete så utvecklades backenden huvudsakligen med Java`,
+        springBoot: `Under min utbildning använde vi Spring Boot för att utveckla bland annat rest API. 
+sedan under mitt senaste arbete så använde vi Spring Boot för backenden.`,
+
+        vue: `I mitt senaste arbete använd jag mig utan vue.js.
+Uppdragen kunde vara allt möjligt mellan små bugfixar,
+till att lägga till flera komponenter och moduler.`,
+        javascript: `Undder min utbildning så arbetade vi främst med JavaScript, CSS och HTML när vi utvecklade frontend.
+På mitt senaste arbete så använde vi endast JavaScript och Vue.js.`,
+        html: `something four times`,
+        css: `CSS och HTML är någont som vi har arbetat med under min utbildning och 
+hänger ihop med JavaScript och därför har jag också arbetat med det under mitt senaste arbete.`,
+        
+        mysql: `Under min utbildning använde vi främst relationsdatabaser och MYSQL.
+Under mitt senaste arbete så var MYSQL det som användes.`,
+        postman: `something seven times`,
+        tablePlus: `something eight times`,
+        
+        git: `Båder under min utbildning samt mitt förra arbete så har vi självklart använda oss utav versionshanteringsverktyg.
+Tillsammans med Git så har jag främst använt Bitbucket och Github.`,
+        bitbucket: `something ten times`,
+        wrike: `something eleven times`
+      }
+
+      return text[name]
     }
   }
 }
@@ -50,11 +135,21 @@ Phone: ` + this.socials.phoneNumber
   white-space: pre-wrap;
 }
 .centering {
-  display:flex;
+  display: flex;
   justify-content: center;
+
+  margin-left: 10rem;
+  margin-right: 10rem;
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+
+  max-width: 768px;
+  max-height: fit-content;
+  margin-left: 50%;
+  margin-right: 50%;
 }
 
-.margin {
-  margin: 15px;
+.margin-between {
+  margin: 1rem;
 }
 </style>
