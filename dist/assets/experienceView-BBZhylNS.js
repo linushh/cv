9fee04eaa553e0cb32c5c010f548391031f72a63
linush <linus_hakansson@outlook.com>
2024-08-01 10:41:24 +0200

    adding dist subtree

---
+++ dist/assets/experienceView-BBZhylNS.js
@@ -0,0 +1,23 @@
+import{_ as i,c,a as o,b as n,s as d,r as l,o as m}from"./index-CYSDeDCI.js";import{c as g}from"./card-BmaJZGK1.js";import"./progress-DcXEM_F8.js";const u="/cv/assets/skolon-Beqc4s9S.jpg",p="/cv/assets/saabVisbyCorvett-CNFAdMar.jpg",h="/cv/assets/bredakraHalloween-B2gHLRzi.jpg",k=d.getSocials(),f={components:{card:g},data(){return{imgSkolon:u,imgBredakra:h,imgSaab:p,socials:k}},methods:{getText(s){return{skolon:`During one of my internships, I worked as a fullstack web developer at Skolon AB.
+
+Technologies that I further developed during my time at Skolon included:
+* Java 17 and Spring Boot
+* JavaScript and Vue.js (Options API)
+* HTML and CSS
+* MySQL
+* Postman
+* Table Plus
+* Git
+* Bitbucket
+* Wrike
+
+A typical day could involve a standup meeting, going through what needed to be done for the day for each specific person, receiving tickets via Wrike, and then carrying out the tasks for the day or days. This could range from finding bugs to implementing entirely new features.
+
+When the task was completed and commits were sent to Bitbucket, there was always a code review where I learned a lot through constructive criticism.`,saab:`During my education as a Java developer, I managed to get a foot in the door at Saab Kockums in Karlskrona, where I worked with IT security under an IT security architect.
+
+I worked with tools like Jira and other internal tools that Saab AB could offer.
+
+We had daily standups, and I got to learn about various information security principles and tactics.
+
+Due to the security classification, I cannot go into much detail.`,bredakra:`I worked in park management and took care of both park work (green spaces) and burial services, such as covering both earth graves with coffins and urn placements.
+The use of various machines was common, such as Walker mowers and Avant.`}[s]}}},v={class:"spacing-around bottom-margin"},S={class:"container"},b={class:"container spacing-around"},w={class:"container spacing-around"};function y(s,r,_,B,e,a){const t=l("card");return m(),c("div",v,[o("div",S,[n(t,{image:e.imgSkolon,title:"Skolon AB - Fullstack Developer (Internship). Östra piren Karlshamn, Sweden",name:a.getText("skolon"),class:"pre-formatted"},null,8,["image","name"])]),o("div",b,[n(t,{image:e.imgSaab,title:"Saab AB - IT Security Specialist (Internship). Kockums Karlskrona, Sweden",name:a.getText("saab"),class:"pre-formatted"},null,8,["image","name"])]),o("div",w,[n(t,{image:e.imgBredakra,title:"Bredåkra - Seasonal Employment, Green Spaces. Bredåkra Kallinge, Sweden",name:a.getText("bredakra"),class:"pre-formatted"},null,8,["image","name"])])])}const A=i(f,[["render",y]]);export{A as default};
