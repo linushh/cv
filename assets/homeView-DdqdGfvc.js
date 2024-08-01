bf51aca1def4cef180b21365f30b46684c00402c
linush <linus_hakansson@outlook.com>
2024-08-01 10:41:24 +0200

    adding dist subtree

---
+++ assets/homeView-DdqdGfvc.js
@@ -0,0 +1,12 @@
+import{a as s,s as n}from"./skillsView-DpatkNBV.js";import{_ as r,c as i,a as c,b as m,s as d,r as l,o as h}from"./index-CYSDeDCI.js";import{c as p}from"./card-BmaJZGK1.js";import"./progress-DcXEM_F8.js";const g=d.getSocials(),u={components:{aboutMe:s,card:p},data(){return{image:n,socials:g}},methods:{getCardText(){return`Hi!
+My name is Linus Håkansson and I am a passionate web developer with a love for creating and exploring.
+When I am not working on building websites, you will often find me at the gym or in some sports venue.
+
+My journey in IT began when I was 14-15 years old and built my first computer at home together with my brother. Since then, I have been hooked and love diving into new technologies and solving problems.
+
+I have worked on several projects that have helped me grow both professionally and personally.
+
+Besides computers, technology, and sports, I am also a big nature lover. I enjoy spending time outdoors. These experiences help me stay inspired and creative.
+
+I created this website to share my projects, my thoughts, and to give you a better picture of who I am as a person.
+I hope you find something interesting here and do not hesitate to contact me if you want to know more or just say hi!`}}},y={class:"background-color"},f={class:"container margin"};function v(o,w,b,_,e,t){const a=l("card");return h(),i("div",y,[c("div",f,[m(a,{image:e.image,name:t.getCardText(),title:e.socials.name,class:"pre-formatted"},null,8,["image","name","title"])])])}const B=r(u,[["render",v]]);export{B as default};
